0bc70a5ab90f248c034abaf9471dc8db4ae848a1
Олег Дидечкин <crm.29431@dz.com>
2023-08-01 22:30:25 +0300

    Visual and UI fixes

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -8,15 +8,27 @@ interface CommentsProps {
 
 const Comments: FC<CommentsProps> = ({ children, depth }) => {
   return (
-    <div
-      className={cn("flex flex-col space-y-4", {
-        "border-l-2 border-light-gray": depth > 0,
-      })}
-      style={{
-        paddingLeft: `${depth * 15}px`,
-      }}>
-      {children}
-    </div>
+    <>
+      <div
+        className={cn("hidden flex-col space-y-4 sm:flex", {
+          "border-l-2 border-light-gray": depth > 0,
+        })}
+        style={{
+          marginLeft: `${depth * 20}px`,
+          paddingLeft: `${depth * 20}px`,
+        }}>
+        {children}
+      </div>
+      <div
+        className={cn("flex flex-col space-y-4 sm:hidden", {
+          "border-l-2 border-light-gray": depth > 0,
+        })}
+        style={{
+          paddingLeft: `${depth * 15}px`,
+        }}>
+        {children}
+      </div>
+    </>
   );
 };
 
